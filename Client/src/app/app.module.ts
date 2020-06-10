7c7323db7bc514d0328b185a88be5f013b2d1e90
Muhammad Karim <muhammadkarim10@gmail.com>
2020-06-10 10:41:26 +0700

    add routes

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http'; // menambhkan library h
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router'; // router
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -11,15 +12,28 @@ import { AuthService } from './_services/auth.service';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes'; // configuration router
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, RegisterComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    RegisterComponent,
+    HomeComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent,
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
     BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent],
--- Client/src/app/nav/nav.component.ts
+++ Client/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,8 @@ export class NavComponent implements OnInit {
 
   constructor(
     public authService: AuthService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {}
@@ -24,6 +26,9 @@ export class NavComponent implements OnInit {
       },
       (error) => {
         this.alertify.error('Gagal Login');
+      },
+      () => {
+        this.router.navigate(['/members']); // add new parameter in subscribe with no parameter
       }
     );
   }
@@ -37,6 +42,7 @@ export class NavComponent implements OnInit {
   logOut() {
     localStorage.removeItem('token');
     this.alertify.message('Logged out');
+    this.router.navigate(['/home']);
 
     // console.log('logged out');
   }
