aa68d5f4e2dc9110a8b0f6a559f9bfb705b636bb
Muhammad Karim <muhammadkarim10@gmail.com>
2020-08-31 13:30:30 +0700

    Creating a Member Edit component

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -26,6 +26,8 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 
 // export function tokenGetter() {
 //   return localStorage.getItem('token');
@@ -49,7 +51,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
     ListsComponent,
     MessagesComponent,
     MemberCardComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -78,7 +81,9 @@ export class CustomHammerConfig extends HammerGestureConfig {
     UserService,
     MemberDetailResolver,
     MemberListResolver,
-    { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
+    MemberEditResolver,
+    { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig },
+
   ],
   bootstrap: [AppComponent],
 })
