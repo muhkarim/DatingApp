229d9c6ff3baf38e567eae8d30fb7a41b488dc83
Muhammad Karim <muhammadkarim10@gmail.com>
2020-11-08 13:03:45 +0700

    Try custom validation in reactive form

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule, ɵɵpureFunctionV, Injectable } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'; // menambhkan library httpclient untuk API
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -64,6 +64,7 @@ export class CustomHammerConfig extends HammerGestureConfig {
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
--- Client/src/app/register/register.component.ts
+++ Client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,25 +12,37 @@ export class RegisterComponent implements OnInit {
   model: any = {};
   // @Input() valuesFromHome: any; --properto untuk api
   @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup;
 
   constructor(
     private authService: AuthService,
     private alertify: AlertifyService
-  ) {}
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('',
+        [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', Validators.required)
+
+    }, this.passwordMatchValidator);
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : { 'mismatch': true };
+  }
 
   register() {
-    this.authService.register(this.model).subscribe(
-      () => {
-        // console.log('registration successfull');
-        this.alertify.success('Registration berhasil');
-      },
-      (error) => {
-        // console.log(error);
-        this.alertify.error(error);
-      }
-    );
+    // this.authService.register(this.model).subscribe(
+    //   () => {
+    //     this.alertify.success('Registration berhasil');
+    //   },
+    //   (error) => {
+    //     this.alertify.error(error);
+    //   }
+    // );
+    console.log(this.registerForm.value);
   }
 
   cancel() {
