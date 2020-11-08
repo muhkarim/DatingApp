db3d1016809667aa8385f3b0035c19d733157f34
Muhammad Karim <muhammadkarim10@gmail.com>
2020-11-08 16:50:39 +0700

    Fixing the Photos issue for newly registered users

--- Client/src/app/members/photo-editor/photo-editor.component.ts
+++ Client/src/app/members/photo-editor/photo-editor.component.ts
@@ -55,6 +55,14 @@ export class PhotoEditorComponent implements OnInit {
           isMain: res.isMain
         };
         this.photos.push(photo);
+
+        // jika new user atau blm ada foto
+        // first upload foto akan jd main foto
+        if (photo.isMain) {
+          this.authService.changeMemberPhoto(photo.url);
+          this.authService.currentUser.photoUrl = photo.url;
+          localStorage.setItem('user', JSON.stringify(this.authService.currentUser));
+        }
       }
     };
   }
